097a35157f8fed7fa4f171056a4b479a0eab623f
bobrov4709 <bobrov4709@mail.ru>
2024-12-15 23:59:26 +0300

    firstStep

---
+++ script.js
@@ -0,0 +1,26 @@
+/*В языке javascript используются переменные. 
+Создание переменных осуществляется с помощью ключего слова let
+
+Can do
+let $ = 'test';
+let $num = 42;
+let num$ = 44;
+let _ = 49;
+let _num = 12;
+let num_ = 24;
+let myNum = '36';
+let num36 = 'My age';
+
+Can not do
+let 42num = '11'
+let my-num = 1
+let false
+let const
+let let
+*/
+
+/*
+We can look what we have on variable
+alert(...)
+console.log(...)
+*/
